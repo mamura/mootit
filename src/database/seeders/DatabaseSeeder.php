4bd9dc4a729a07654bc2d6a3680ab4dbab85336c
Mamura <marcio.mota@gmail.com>
2025-07-07 09:43:55 -0300

    estrutura | models | migrations | seeders

---
+++ src/database/seeders/DatabaseSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Brand;
+use App\Models\Category;
+use App\Models\Product;
+use App\Models\User;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        Category::factory(10)->create();
+        Brand::factory(10)->create();
+
+        Product::factory(100)->create([
+            'category_id'   => fn () => Category::inRandomOrder()->first()->id,
+            'brand_id'      => fn () => Brand::inRandomOrder()->first()->id,
+        ]);
+    }
+}
---
+++ src/resources/views/layouts/app.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <title>Busca de Produtos</title>
+    @livewireStyles
+</head>
+<body>
+    <div class="container">
+        {{ $slot }}
+    </div>
+
+    @livewireScripts
+</body>
+</html>
